84d4a798850f9d317f0408ab11f14ceb286203f1
Bob Tribbia <btribbia@newrelic.com>
2017-02-18 15:43:48 -0800

    Added scraping/viewing for capmng.com

---
+++ scrape/cap.js
@@ -0,0 +1,121 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var results = [];
+
+var cap = function(url) {this.url = url; this.results = []; }
+
+function scrapeListings(urls) {
+
+
+	var outstandingRequests = 0;
+
+
+	urls.forEach(function(listing, i) {
+		outstandingRequests++;
+		request(listing, function(error, response, html){
+			outstandingRequests--;
+
+			var $ = cheerio.load(html);
+
+			var pic = [];
+			var locs = {lat: 0, long: 0};
+
+			//Grab lat/lng from google maps js
+			$('script').each(function(i,js) {
+				if ($(js).html().indexOf('latitude') > -1) {
+					var lat = $(js).html().split('*/')[1].split('\n')[1];
+					var lng = $(js).html().split('*/')[1].split('\n')[2];
+
+					locs.lat = parseFloat(lat.split('= ')[1].replace(';',''));
+					locs.long = parseFloat(lng.split('= ')[1].replace(';',''));
+				}
+
+			});
+
+			$('.rsImg').each(function(i,img) {
+				var imgUrl = 'http://www.capmng.com' + $(img).attr('src');
+				pic.push(imgUrl);
+			});
+
+			var details = $('.details-title').html().split('<br>');
+			var bed = (details[0].indexOf('Studio') > -1) ? 0 : details[0].replace(' Bedroom','');
+			var rent = parseInt(details[1].replace('$','').replace(',',''));
+			var title = $('.aptcontent').children('h2').html();
+			var address = $('.aptcontent').children('.address').html().replace('<br>',' ');
+			var avail = $('.availdate').html();
+
+			
+	    	
+	    	var body = "";
+	        var link = listing;
+	        var sqft = "";
+	        var bedba = "";
+	        var bath = "";
+	        var desc = "";
+	        var amen = "";
+
+	        var result = {
+	        	pic: pic,
+	        	site: "http://www.capmng.com/availability/details.php",
+	        	title: title,
+	        	link: link,
+	        	rent: parseInt(rent),
+	        	sqft: parseInt(sqft),
+	        	bedbath: bedba,
+	        	bed: parseInt(bed),
+	        	bath: parseFloat(bath),
+	        	address: address,
+	        	availability: avail,
+	        	description: desc,
+	        	amenities: amen,
+	        	lat: locs.lat,
+	        	long: locs.long
+	        };
+
+	        results.push(result);
+
+
+		});
+
+		if (outstandingRequests == 0) {
+			console.log('all done!');
+		}
+	}); 
+
+}
+
+
+cap.prototype.scrape = function() {
+
+   var urls = [];
+
+
+   request(this.url, function(error, response, html){
+
+   		if (!error) {
+   			var $ = cheerio.load(html);
+
+   			$('.sub-aptlist').each(function(i,section) {
+   				if ($(this).attr('class').indexOf('hidden') == -1) {
+   					$(this).children('li').each(function(j,listing) {
+   						var listingURL = 'http://www.capmng.com/availability/details.php' + $(listing).children('a').attr('href');
+   						urls.push(listingURL);
+   					});
+   				}
+   			});
+   			
+   			scrapeListings(urls);
+   		}
+   });
+
+}
+
+
+// //expose results array
+cap.prototype.getResults = function() {
+	return results;
+}
+
+
+module.exports = cap;
+
--- server.js
+++ server.js
@@ -6,15 +6,19 @@ var path = require('path');
 //scrape libaries
 var Appfolio = require('./scrape/appfolio.js');
 var Craigslist = require('./scrape/craigslist.js');
+var Cap = require('./scrape/cap.js');
 
 //scrape on startup
-var appfolioArray = ['manifaust','fox','ascendpm','interwest','capitalpropertymgmt','rpmpdx','adi','realtysolutions','kbcmgmt','peak','propmhomes','realpronw','garciagrp','firstclassproperty','popm','gordonproperties','circum','profast','holland','sharpproperty','associatedpm','performance','alder','voss','gebhardtpm','mirrorpropertymanagement','dnpm','anchor','bbmgmt','sullivanmgmt','twentythird','milestonepropertymgmt','acornpm','jmre','agrentalmgmt','gridpm','livingroomproperty','regencypropertymgt','carefree'];
+var appfolioArray = ['rmspdx','manifaust','fox','ascendpm','interwest','capitalpropertymgmt','rpmpdx','adi','realtysolutions','kbcmgmt','peak','propmhomes','realpronw','garciagrp','firstclassproperty','popm','gordonproperties','circum','profast','holland','sharpproperty','associatedpm','performance','alder','voss','gebhardtpm','mirrorpropertymanagement','dnpm','anchor','bbmgmt','sullivanmgmt','twentythird','milestonepropertymgmt','acornpm','jmre','agrentalmgmt','gridpm','livingroomproperty','regencypropertymgt','carefree'];
 //var appfolioArray = ['manifaust'];
 var appfolio = new Appfolio(appfolioArray);
 appfolio.scrape();
 
 var craigslist = new Craigslist('https://portland.craigslist.org/search/apa?availabilityMode=0&bedrooms=1&laundry=1&laundry=4&max_price=1380&min_price=800&postal=97212&postedToday=1&search_distance=10');
 craigslist.scrape();
+
+var cap = new Cap('http://www.capmng.com/availability/details.php');
+cap.scrape();
 
 
 app.use('/', express.static(path.join(__dirname + '/public')));
@@ -25,7 +29,9 @@ app.get('/appfolio', function(req, res){
 app.get('/craigslist', function(req, res){
 	res.send(craigslist.getResults());
 });
-
+app.get('/cap', function(req, res){
+	res.send(cap.getResults());
+});
 
 app.listen('8081')
 console.log('Listening on 8081');
