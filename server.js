71ff779ab0717b5c73d41024b1a7146962618696
Robert Tribbia <rtribbia@gmail.com>
2017-02-24 20:33:15 -0800

    refactored appfolio scrape module

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var path = require('path');
 
 
 //scrape libaries
-var Appfolio = require('./src/scrape/appfolio.js');
+var appfolio = require('./src/scrape/appfolio.js');
 var Craigslist = require('./src/scrape/craigslist.js');
 var Cap = require('./src/scrape/cap.js');
 var Princeton = require('./src/scrape/princeton.js');
 
 
 //scrape config
-var appfolioArray = ['rmspdx','manifaust','fox','ascendpm','interwest','capitalpropertymgmt','rpmpdx','adi','realtysolutions','kbcmgmt','peak','propmhomes','realpronw','garciagrp','firstclassproperty','popm','gordonproperties','circum','profast','holland','sharpproperty','associatedpm','performance','alder','voss','gebhardtpm','mirrorpropertymanagement','dnpm','anchor','bbmgmt','sullivanmgmt','twentythird','milestonepropertymgmt','acornpm','jmre','agrentalmgmt','gridpm','livingroomproperty','regencypropertymgt','carefree'];
-var appfolio = new Appfolio(appfolioArray);
+//var appfolioArray = ['rmspdx','manifaust','fox','ascendpm','interwest','capitalpropertymgmt','rpmpdx','adi','realtysolutions','kbcmgmt','peak','propmhomes','realpronw','garciagrp','firstclassproperty','popm','gordonproperties','circum','profast','holland','sharpproperty','associatedpm','performance','alder','voss','gebhardtpm','mirrorpropertymanagement','dnpm','anchor','bbmgmt','sullivanmgmt','twentythird','milestonepropertymgmt','acornpm','jmre','agrentalmgmt','gridpm','livingroomproperty','regencypropertymgt','carefree'];
+var appfolioArray = ['rmspdx','manifaust'];
 var craigslist = new Craigslist('https://portland.craigslist.org/search/apa?availabilityMode=0&bedrooms=1&laundry=1&laundry=4&max_price=1380&min_price=800&postal=97212&postedToday=1&search_distance=10');
 var cap = new Cap('http://www.capmng.com/availability/details.php');
 var princeton = new Princeton('http://www.princetonproperty.com/map_view/');
@@ -21,10 +21,15 @@ var princeton = new Princeton('http://www.princetonproperty.com/map_view/');
 
 
 //scrape execute
-// appfolio.scrape();
- craigslist.scrape();
+appfolio.scrape(appfolioArray);
+//craigslist.scrape();
 // cap.scrape();
 // princeton.generateQuery();
+
+// Promise.all([appfolio.scrape()]).then(function(results) {
+// 	console.log(results + ' appfolio results found');
+// });
+
 
 app.use('/', express.static(path.join(__dirname + '/public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules/'))
--- src/scrape/appfolio.js
+++ src/scrape/appfolio.js
@@ -1,119 +1,127 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var appfolio = function(siteArray) {this.siteArray = siteArray; this.results = []; }
+var appfolio = {
+	results: [],
+	urls: [],
+	outstandingRequests: 0,
+	googleMapLocs: []
+}
+
 
 //main scrape method
-appfolio.prototype.scrape = function() {
+appfolio.scrape = function(siteArray) {
 
-	var outstandingRequests = 0;
-
-	var urls = [];
-	var json = [];
-
-	this.siteArray.forEach(function(app,i) {
-		var url = 'https://' + app + '.appfolio.com/listings/listings';
-	    urls.push(url);
-	});
-
-	var results = this.results;
-
-
-	urls.forEach(function(url,i) {
-
-		outstandingRequests++;
-
-		request(url, function(error, response, html){
-
-			outstandingRequests--;
-
-		    if(!error) {
-		        var $ = cheerio.load(html);
-		        var locationArray;
-
-		        //cycle through <scripts> to find googleMaps script
-			    $('script').each(function(i,element) {
-			    		var rawScript = $(this).html();
-
-			    		//markers[] array contains all lat/long data.
-				     	if (rawScript.indexOf('markers:') > -1) {
-				     		var rawJSON = rawScript.substring(rawScript.indexOf('['),rawScript.indexOf(']')+1);
-				     		locationArray = JSON.parse(rawJSON);
-				     	}
-			    });
-			   
-
-			    //Parse each div.result
-			    $('.result').each(function(i, element){
-			    	var locs = {lat: 0, long: 0};
-			    	var pic = [$('.js-listing-image',this).attr('data-original')];
-			    	var body = $(this).children().eq(1);
-			        var link = $('.js-hand-hidden-link-to-detail',this).attr('href');
-			        var title = $('.js-listing-title',this).text()
-			        var rent = $(body).children().eq(0).children().eq(0).children().eq(1).text().replace('$','').replace(',','');
-			        var sqft = $(body).children().eq(0).children().eq(1).children().eq(1).text();
-			        var bedba = $(body).children().eq(0).children().eq(2).children().eq(1).text();
-			        var parsed = bedba.replace(/ /g,'').replace('bd','').replace('ba','').split('/');
-			        var bed = parsed[0];
-			        var bath = parsed[1];
-			        var address = $('.js-listing-address',this).text();
-			        var avail = $('.js-listing-available',this).text();
-			        var desc = $('.js-listing-description',this).text();
-			        var amen = $(body).children().eq(5).text();
-			        var mapId = $('.js-listing-map-view-link',this).attr('data-listing-id');
-
-			        if (mapId && locationArray.length) {
-			        	locs = findLocs(locationArray,mapId);
-			        }
-
-			        var result = {
-			        	pic: pic,
-			        	site: url,
-			        	title: title,
-			        	link: url.replace('/listings/listings',link),
-			        	rent: parseInt(rent),
-			        	sqft: parseInt(sqft),
-			        	bedbath: bedba,
-			        	bed: parseInt(bed),
-			        	bath: parseFloat(bath),
-			        	address: address,
-			        	availability: avail,
-			        	description: desc,
-			        	amenities: amen,
-			        	mapId: mapId,
-			        	lat: locs.lat,
-			        	long: locs.long
-			        };
-
-			        if (result.rent > 700) {
-			       		results.push(result);
-			        }
-			    });
-
-			}
-
-	        if (outstandingRequests == 0) {	
-	       		console.log(urls.length + ' Appfolio sites scraped.');
-	       		console.log(results.length + ' Appfolio listings found.');
-	   		}
+		siteArray.forEach(function(app,i) {
+			var url = 'https://' + app + '.appfolio.com/listings/listings';
+		    appfolio.urls.push(url);
 		});
-	});
+
+		appfolio.urls.forEach(scrapeURL);
+
 }
 
 //expose results array
-appfolio.prototype.getResults = function() {
-	return this.results;
+appfolio.getResults = function() {
+	return appfolio.results;
 }
 
-//Search google maps array for lat/long
-function findLocs (larray,id) {
-	for (var i = 0; i < larray.length; i++) {
-		if (larray[i].listing_id == id) {
-			return {lat: larray[i].latitude, long: larray[i].longitude};
+
+function findLocs (id) {
+	for (var i = 0; i < appfolio.googleMapLocs.length; i++) {
+		if (appfolio.googleMapLocs[i].listing_id == id) {
+			return {lat: appfolio.googleMapLocs[i].latitude, long: appfolio.googleMapLocs[i].longitude};
 		}
 	}
 	return {lat: 0, long: 0};
 }	
+
+function scrapeURL (url,i) {
+
+	appfolio.outstandingRequests++;
+
+	request(url, function(error, response, html){
+
+		appfolio.outstandingRequests--;
+
+	    if(!error) {
+	        var $ = cheerio.load(html);
+
+	        //cycle through <scripts> to find googleMaps script
+		    $('script').each(function(element,i){
+				var rawScript = $(this).html();
+				//markers[] array contains all lat/long data.
+			 	if (rawScript.indexOf('markers:') > -1) {
+			 		var rawJSON = rawScript.substring(rawScript.indexOf('['),rawScript.indexOf(']')+1);
+			 		appfolio.googleMapLocs = JSON.parse(rawJSON);
+			 	}
+		    });
+		   
+		    //Parse each div.result
+		    $('.result').each(function(i,element){
+		    	parseResult(element,url);
+		    });
+
+		}
+
+        if (appfolio.outstandingRequests == 0) {	
+       		console.log(appfolio.urls.length + ' Appfolio sites scraped.');
+       		console.log(appfolio.results.length + ' Appfolio listings found.');
+       		//return resolve(results.length);
+   		}
+	});
+}
+
+function parseResult(element,url) {
+
+	var $ = cheerio.load(element);
+
+
+	var locs = {lat: 0, long: 0};
+	var pic = [$('.js-listing-image',element).attr('data-original')];
+	var body = $(element).children().eq(1);
+    var link = $('.js-hand-hidden-link-to-detail',element).attr('href');
+    var title = $('.js-listing-title',element).text()
+    var rent = $(body).children().eq(0).children().eq(0).children().eq(1).text().replace('$','').replace(',','');
+    var sqft = $(body).children().eq(0).children().eq(1).children().eq(1).text();
+    var bedba = $(body).children().eq(0).children().eq(2).children().eq(1).text();
+    var parsed = bedba.replace(/ /g,'').replace('bd','').replace('ba','').split('/');
+    var bed = parsed[0];
+    var bath = parsed[1];
+    var address = $('.js-listing-address',element).text();
+    var avail = $('.js-listing-available',element).text();
+    var desc = $('.js-listing-description',element).text();
+    var amen = $(body).children().eq(5).text();
+    var mapId = $('.js-listing-map-view-link',element).attr('data-listing-id');
+
+    if (mapId && appfolio.googleMapLocs.length) {
+    	locs = findLocs(mapId);
+    }
+
+    var result = {
+    	pic: pic,
+    	site: url,
+    	title: title,
+    	link: url.replace('/listings/listings',link),
+    	rent: parseInt(rent),
+    	sqft: parseInt(sqft),
+    	bedbath: bedba,
+    	bed: parseInt(bed),
+    	bath: parseFloat(bath),
+    	address: address,
+    	availability: avail,
+    	description: desc,
+    	amenities: amen,
+    	mapId: mapId,
+    	lat: locs.lat,
+    	long: locs.long
+    };
+
+    if (result.rent > 700) {
+   		appfolio.results.push(result);
+    }
+
+}
 
 
 module.exports = appfolio;
